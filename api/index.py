c4a0c6d85f2fff5ceb1c994918467e9c3bb66a71
GPScript67 <poulet.guillaume@gmail.com>
2025-02-07 12:46:51 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -29,39 +29,58 @@ def home():
     categories = list(VOCABULARY.keys())
     return render_template('index.html', categories=categories)
 
-@app.route('/get_quiz', methods=['POST'])
-def get_quiz():
+@app.route('/get_words', methods=['POST'])
+def get_words():
     data = request.json
     categories = data.get('categories', [])
-    num_questions = int(data.get('num_questions', 10))
-    direction = data.get('direction', 'jp_to_en')
+    mode = data.get('mode', 'quiz')
     
-    if categories:
-        all_words = [(jp, en, cat) for cat in categories 
-                     for jp, en in VOCABULARY.get(cat, [])]
-    else:
-        all_words = [(jp, en, cat) for cat, words in VOCABULARY.items() 
-                     for jp, en in words]
-    
-    num_questions = min(num_questions, len(all_words))
-    questions = random.sample(all_words, num_questions)
-    
-    quiz_questions = []
-    for jp, en, category in questions:
-        if direction == 'jp_to_en':
-            quiz_questions.append({
-                'question': jp,
-                'answer': en,
-                'category': category
-            })
+    if mode == 'flashcards':
+        # For flashcards, we want all words from selected categories
+        if categories:
+            words = [(jp, en, cat) for cat in categories 
+                    for jp, en in VOCABULARY.get(cat, [])]
         else:
-            quiz_questions.append({
-                'question': en,
-                'answer': jp,
-                'category': category
-            })
+            words = [(jp, en, cat) for cat, words in VOCABULARY.items() 
+                    for jp, en in words]
+        
+        return jsonify([{
+            'japanese': jp,
+            'english': en,
+            'category': cat
+        } for jp, en, cat in words])
     
-    return jsonify(quiz_questions)
+    else:
+        # Original quiz logic
+        num_questions = int(data.get('num_questions', 10))
+        direction = data.get('direction', 'jp_to_en')
+        
+        if categories:
+            all_words = [(jp, en, cat) for cat in categories 
+                        for jp, en in VOCABULARY.get(cat, [])]
+        else:
+            all_words = [(jp, en, cat) for cat, words in VOCABULARY.items() 
+                        for jp, en in words]
+        
+        num_questions = min(num_questions, len(all_words))
+        questions = random.sample(all_words, num_questions)
+        
+        quiz_questions = []
+        for jp, en, category in questions:
+            if direction == 'jp_to_en':
+                quiz_questions.append({
+                    'question': jp,
+                    'answer': en,
+                    'category': category
+                })
+            else:
+                quiz_questions.append({
+                    'question': en,
+                    'answer': jp,
+                    'category': category
+                })
+        
+        return jsonify(quiz_questions)
 
 if __name__ == '__main__':
     app.run(debug=True)
